7f9602eff9092ccd159dbd7adcbbf90317efcbc2
frame88 <fleuzzi97@gmail.com>
2022-03-02 11:47:40 +0100

    utilizzo metodi isalpha() e islower()

--- 3.5.py
+++ 3.5.py
@@ -1,1 +1,9 @@
+word_1 = input('Iinserisci una stringa: ')
+print(word_1[0],word_1[-1])
+
+while word_1.isalpha() == False or word_1.islower() == False:
+    word_1 = input('Iinserisci una stringa: ')
+    print(word_1[0],word_1[-1])
+
+
 
